b44d85ae63661b3d6dcfb86174b793a9538617b6
Evan Lin <evan.if.lin@linecorp.com>
2024-09-10 13:25:55 +0800

    Refactor main.py and langtools.py for improved message formatting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
 
-from fastapi import FastAPI
-from fastapi import Request
-
+from fastapi import FastAPI, Request
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
@@ -17,25 +15,20 @@ github_token = os.getenv("GITHUB_TOKEN")
 repo_name = os.getenv("REPO_NAME")
 repo_owner = os.getenv("REPO_OWNER")
 
-# check if the environment variables are set
-if not linebot_token:
-    print("LINE_BOT_TOKEN is not set")
-    exit(1)
-if not linebot_user_id:
-    print("LINE_USER_ID is not set")
-    exit(1)
-if not google_api_key:
-    print("GOOGLE_API_KEY is not set")
-    exit(1)
-if not github_token:
-    print("GITHUB_TOKEN is not set")
-    exit(1)
-if not repo_name:
-    print("REPO_NAME is not set")
-    exit(1)
-if not repo_owner:
-    print("REPO_OWNER is not set")
-    exit(1)
+# Check if the environment variables are set
+required_env_vars = {
+    "LINE_BOT_TOKEN": linebot_token,
+    "LINE_USER_ID": linebot_user_id,
+    "GOOGLE_API_KEY": google_api_key,
+    "GITHUB_TOKEN": github_token,
+    "REPO_NAME": repo_name,
+    "REPO_OWNER": repo_owner,
+}
+
+for var_name, var_value in required_env_vars.items():
+    if not var_value:
+        print(f"{var_name} is not set")
+        exit(1)
 
 # Initialize the FastAPI app for LINEBot
 app = FastAPI()
@@ -43,68 +36,60 @@ app = FastAPI()
 
 @app.get("/ds")
 def github_issue_daily_summarization():
-    # get from console
-    try:
-        print("-------github_issue_daily_summarization------")
-        text = summarized_yesterday_github_issues(github_token, repo_owner, repo_name)
-        print(text)
-        send_msg(linebot_user_id, linebot_token, text)
-        return "OK"
-    except Exception as e:
-        print(e)
-        return "Error"
+    return handle_summarization(
+        title="GitHub Issues",
+        url=None,
+        summarization_func=lambda: summarized_yesterday_github_issues(
+            github_token, repo_owner, repo_name
+        ),
+    )
 
 
 @app.post("/hn")
-async def hacker_news_sumarization(request: Request):
-    try:
-        print("-------hacker_news_sumarization------")
-        data = await request.json()
-        title = data.get("title")
-        url = data.get("url")
-        print(f"Title: {title}\nURL: {url}")
-        result = summarize_with_sherpa(url)
-        if not result:
-            # Handle the error case, e.g., log the error or set a default message
-            result = "An error occurred while summarizing the document."
-            print(result)
-        elif len(result) > 2000:
-            result = summarize_text(result)
-            print(result)
-            out_text = f"{title} \n{url} \n{result}"
-            send_msg(linebot_user_id, linebot_token, out_text)
-        return "OK"
-    except Exception as e:
-        print(e)
-        return "Error"
+async def hacker_news_summarization(request: Request):
+    data = await request.json()
+    title = data.get("title")
+    url = data.get("url")
+    return handle_summarization(title, url, summarize_with_sherpa)
 
 
 @app.post("/hf")
-async def huggingface_paper_sumarziation(request: Request):
-    try:
-        print("-------HuggingFace_Paper_sumarization------")
-        data = await request.json()
-        title = data.get("title")
-        papertocode_url = data.get("url")
-        print(f"Papertocode URL: {papertocode_url}")
-        # replace "https://paperswithcode.com/papers/2408.16046" to "https://huggingface.co/papers/2408.16046"
-        url = papertocode_url.replace("paperswithcode.com", "huggingface.co")
+async def huggingface_paper_summarization(request: Request):
+    data = await request.json()
+    title = data.get("title")
+    papertocode_url = data.get("url")
+    url = replace_domain(papertocode_url, "paperswithcode.com", "huggingface.co")
+    return handle_summarization(title, url, summarize_with_sherpa)
 
-        print(f"Title: {title}\nURL: {url}")
-        result = summarize_with_sherpa(url)
-        if not result:
-            # Handle the error case, e.g., log the error or set a default message
-            result = "An error occurred while summarizing the document."
-            print(result)
-        elif len(result) > 2000:
-            result = summarize_text(result)
-            print(result)
-            out_text = f"{title} \n{url} \n{result}"
+
+def handle_summarization(title, url, summarization_func):
+    try:
+        print(f"-------{title} Summarization------")
+        if url:
+            print(f"Title: {title}\nURL: {url}")
+        result = summarization_func() if not url else summarization_func(url)
+        result = handle_summary_result(result)
+        if result:
+            out_text = f"{title} \n{url} \n{result}" if url else result
             send_msg(linebot_user_id, linebot_token, out_text)
         return "OK"
     except Exception as e:
         print(e)
         return "Error"
+
+
+def handle_summary_result(result):
+    if not result:
+        result = "An error occurred while summarizing the document."
+        print(result)
+    elif len(result) > 2000:
+        result = summarize_text(result)
+        print(result)
+    return result
+
+
+def replace_domain(url, old_domain, new_domain):
+    return url.replace(old_domain, new_domain)
 
 
 def send_msg(linebot_user_id, linebot_token, text):
